4c373a1f39f4768a98fd83024827e07c6485c304
Aaron Ferber <an9@ornl.gov>
2017-05-17 15:39:49 -0400

    Modified test scripts to prevent racing with kafka creating topics.

--- do_test.sh
+++ do_test.sh
@@ -2,8 +2,19 @@
 
 ./start_kafka.sh
 
-# Give kafka time to create the topics.
-sleep 10
+# Wait until Kafka creates our topics.
+while true; do
+    ntopics=$(docker exec jpocvdp_kafka_1 /opt/kafka/bin/kafka-topics.sh --list --zookeeper 172.17.0.1 | wc -l)
+
+    if [[ $ntopics == "2" ]]; then 
+        echo 'Found 2 topics:'
+        docker exec jpocvdp_kafka_1 /opt/kafka/bin/kafka-topics.sh --list --zookeeper 172.17.0.1 2> /dev/null
+        
+        break   
+    fi
+
+    sleep 1
+done
 
 MAP_FILE=data/I_80.edges
 DATA_FILE=data/I_80_test.json
