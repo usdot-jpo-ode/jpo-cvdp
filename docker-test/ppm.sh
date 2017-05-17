1a7c6c57a7c0930227030ee9f3ec8a9f141fd0d4
Jason M. Carter <carterjm@ornl.gov>
2017-05-17 10:05:41 -0400

    attempting to change some of the docker scripts so the tests pass.

--- docker-test/ppm.sh
+++ docker-test/ppm.sh
@@ -4,4 +4,5 @@ export LD_LIBRARY_PATH=/usr/local/lib
 broker=172.17.0.1:9092
 
 # Start the DI tool.
-/cvdi-stream-build/bsmjson_privacy -g 0 -b $broker -c /ppm_data/config.properties 2> priv.err
+# /cvdi-stream-build/bsmjson_privacy -g 0 -b $broker -c /ppm_data/config.properties 2> priv.err
+/cvdi-stream-build/ppm -g 0 -b $broker -c /ppm_data/config.properties 2> priv.err
