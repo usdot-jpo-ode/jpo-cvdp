311fb84c649c3dd42816faaffd7cead30866f6fa
572682 <musavi_hamid@bah.com>
2017-05-18 13:44:16 -0400

    ODE-290 Minor updates for integration with ODE.
    The changes include:
    1. Adding DOCKER_HOST_IP to the set up so that it can be easily deployed to any user environment.
    2. Adding a shared folder so configuration and data files can be managed more easily from outside the container.
    3. Added new ppm.properties file with privacy.filter.geofence.mapfile pointing to a file in /ppm_data/data folder

--- docker-test/ppm.sh
+++ docker-test/ppm.sh
@@ -2,4 +2,4 @@
 export LD_LIBRARY_PATH=/usr/local/lib
 
 # Start the DI tool.
-/cvdi-stream-build/bsmjson_privacy -g 0 -b ${DOCKER_HOST_IP}:9092 -c /ppm_data/config/ppm.properties 2> priv.err
+/cvdi-stream-build/ppm -g 0 -b ${DOCKER_HOST_IP}:9092 -c /ppm_data/config/ppm.properties 2> priv.err
