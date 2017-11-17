a2e649b8daba951e640b0c4d24b3917364f99857
Tony Chen <tonychen091@gmail.com>
2017-11-16 22:03:51 -0700

    Updated to use an envrionmental variable for configuration

--- docker-test/ppm.sh
+++ docker-test/ppm.sh
@@ -2,4 +2,6 @@
 export LD_LIBRARY_PATH=/usr/local/lib
 
 # Start the DI tool.
-/cvdi-stream-build/ppm -m /ppm_data/road_file.csv -c /ppm_data/config.properties -b ${DOCKER_HOST_IP}:9092 -o end
+
+/cvdi-stream-build/ppm -m /ppm_data/road_file.csv -c /ppm_data/${PPM_CONFIG_FILE} -b ${DOCKER_HOST_IP}:9092 
+
