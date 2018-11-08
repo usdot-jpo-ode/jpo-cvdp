5d0b308570f0047df0435d05fbcda5f530e7d4b2
Hamid Musavi <musavi_hamid@bah.com>
2018-11-08 10:21:44 -0500

    Due to issues encountered Docker sharing volumes with host machine on corporate laptops, these changes will include configuration files within the docker image.

--- docker-test/ppm.sh
+++ docker-test/ppm.sh
@@ -3,5 +3,5 @@ export LD_LIBRARY_PATH=/usr/local/lib
 
 # Start the DI tool.
 
-/cvdi-stream-build/ppm -m /ppm_data/road_file.csv -c /ppm_data/${PPM_CONFIG_FILE} -b ${DOCKER_HOST_IP}:9092 
+/cvdi-stream-build/ppm -c /cvdi-stream/config/${PPM_CONFIG_FILE} -m /cvdi-stream/config/${PPM_MAP_FILE} -b ${DOCKER_HOST_IP}:9092 
 
