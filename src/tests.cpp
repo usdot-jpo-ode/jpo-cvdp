3835df153a025f217725e7baf86cafddfc5b6b95
Daniel McCoy Stephenson <dmccoystephenson@gmail.com>
2023-01-18 10:41:10 -0700

    Added/modified checks in general redaction tests.

--- src/tests.cpp
+++ src/tests.cpp
@@ -1454,7 +1454,7 @@ TEST_CASE( "BSMHandler JSON Error Checking", "[ppm][filtering][error]" ) {
     }
 }
 
-TEST_CASE( "Rapidjson Redactor Redact All Instances Of Member By Name", "[ppm][redaction][rapidjsonredactor]" ) {
+TEST_CASE( "RapidjsonRedactor Redact All Instances Of Member By Name", "[ppm][redaction][rapidjsonredactor][redactallinstancesbyname]" ) {
     RapidjsonRedactor rapidjsonRedactor;
 
     const char* jsonString = "{\"metadata\":{\"latency\":1,\"logFileName\":\"wsmpforward.coer\",\"payloadType\":\"us.dot.its.jpo.ode.model.OdeBsmPayload\",\"receivedAt\":\"2017-08-02T19:56:45.822Z[UTC]\",\"sanitized\":false,\"schemaVersion\":1,\"serialId\":{\"bundleId\":4,\"bundleSize\":1,\"recordId\":2,\"serialNumber\":0,\"streamId\":\"0bfda39b-0bf1-4e2e-a1f1-b858426f7408\"},\"validSignature\":false},\"payload\":{\"data\":{\"coreData\":{\"accelSet\":{\"accelYaw\":0},\"accuracy\":{\"semiMajor\":12.7,\"semiMinor\":12.7},\"brakes\":{\"abs\":\"unavailable\",\"auxBrakes\":\"unavailable\",\"brakeBoost\":\"unavailable\",\"scs\":\"unavailable\",\"traction\":\"unavailable\",\"wheelBrakes\":{\"leftFront\":false,\"leftRear\":false,\"rightFront\":false,\"rightRear\":false,\"unavailable\":true}},\"heading\":321.0125,\"id\":\"G1\",\"msgCnt\":1,\"position\":{\"elevation\":154.7,\"latitude\":35.94911,\"longitude\":-83.928343},\"secMark\":36799,\"size\":{\"length\":250,\"width\":150},\"speed\":22.1},\"partII\":[{\"id\":\"VEHICLESAFETYEXT\",\"value\":{\"pathHistory\":{\"crumbData\":[{\"elevationOffset\":-19.8,\"latOffset\":7.55e-5,\"lonOffset\":0.0002609,\"timeOffset\":32.2},{\"elevationOffset\":-25.8,\"latOffset\":7.32e-5,\"lonOffset\":0.0003135,\"timeOffset\":34},{\"elevationOffset\":-34.5,\"latOffset\":0.0001027,\"lonOffset\":0.0004479,\"timeOffset\":37.2},{\"elevationOffset\":-128.2,\"latOffset\":0.000232,\"lonOffset\":0.0011832,\"timeOffset\":73.44}]},\"pathPrediction\":{\"confidence\":50,\"radiusOfCurve\":0}}},{\"id\":\"SUPPLEMENTALVEHICLEEXT\",\"value\":{\"classDetails\":{\"fuelType\":\"UNKNOWNFUEL\",\"hpmsType\":\"NONE\",\"keyType\":0,\"regional\":[],\"role\":\"BASICVEHICLE\"},\"regional\":[],\"vehicleData\":{\"bumpers\":{\"front\":0.5,\"rear\":0.6},\"height\":1.9},\"weatherProbe\":{},\"accelLong\":\"test\",\"accelLat\":\"test\",\"accelVert\":\"test\",\"yawRate\":\"test\",\"steeringWheelAngle\":\"test\",\"leftTurnSignalOn\":\"test\",\"rightTurnSignalOn\":\"test\",\"hazardSignalOn\":\"test\",\"fogLightOn\":\"test\",\"lowBeamHeadlightsOn\":\"test\",\"highBeamHeadlightsOn\":\"test\",\"automaticLightControlOn\":\"test\",\"daytimeRunningLightsOn\":\"test\",\"parkingLightsOn\":\"test\",\"wiperStatusFront\":\"test\",\"wiperStatusRear\":\"test\",\"wiperRateFront\":\"test\",\"wiperRateRear\":\"test\",\"eventAirBagDeployment\":\"test\",\"sunSensor\":\"test\",\"coefficientOfFriction\":\"test\",\"ambientAirTemperature\":\"test\",\"ambientAirPressure\":\"test\",\"transmissionState\":\"test\",\"vehicleSpeed\":\"test\",\"antiLockBrakeStatus\":\"test\",\"stabilityControlStatus\":\"test\",\"tractionControlStatus\":\"test\",\"brakeBoostApplied\":\"test\",\"brakeAppliedStatus\":\"test\",\"auxiliaryBrakeStatus\":\"test\",\"tirePressure\":\"test\",\"acceleratorPedalPosition\":\"test\",\"brakePedalPosition\":\"test\",\"disabledVehicle\":\"test\",\"stalledVehicle\":\"test\",\"airBagDeployment\":\"test\"}}]},\"dataType\":\"us.dot.its.jpo.ode.plugin.j2735.J2735Bsm\",\"schemaVersion\":1},\"schemaVersion\":1}";
@@ -1541,7 +1541,7 @@ TEST_CASE( "Rapidjson Redactor Redact All Instances Of Member By Name", "[ppm][r
     REQUIRE(numMembersPresentAfterRedaction == 0);
 }
 
-TEST_CASE( "Rapidjson Redactor Redact Member By Path", "[ppm][redaction][rapidjsonredactor]" ) {
+TEST_CASE( "RapidjsonRedactor Redact Member By Path", "[ppm][redaction][rapidjsonredactor][redactbypath]" ) {
     RapidjsonRedactor rapidjsonRedactor;
 
     const char* jsonString = "{\"metadata\":{\"latency\":1,\"logFileName\":\"wsmpforward.coer\",\"payloadType\":\"us.dot.its.jpo.ode.model.OdeBsmPayload\",\"receivedAt\":\"2017-08-02T19:56:45.822Z[UTC]\",\"sanitized\":false,\"schemaVersion\":1,\"serialId\":{\"bundleId\":4,\"bundleSize\":1,\"recordId\":2,\"serialNumber\":0,\"streamId\":\"0bfda39b-0bf1-4e2e-a1f1-b858426f7408\"},\"validSignature\":false},\"payload\":{\"data\":{\"coreData\":{\"accelSet\":{\"accelYaw\":0},\"accuracy\":{\"semiMajor\":12.7,\"semiMinor\":12.7},\"brakes\":{\"abs\":\"unavailable\",\"auxBrakes\":\"unavailable\",\"brakeBoost\":\"unavailable\",\"scs\":\"unavailable\",\"traction\":\"unavailable\",\"wheelBrakes\":{\"leftFront\":false,\"leftRear\":false,\"rightFront\":false,\"rightRear\":false,\"unavailable\":true}},\"heading\":321.0125,\"id\":\"G1\",\"msgCnt\":1,\"position\":{\"elevation\":154.7,\"latitude\":35.94911,\"longitude\":-83.928343},\"secMark\":36799,\"size\":{\"length\":250,\"width\":150},\"speed\":22.1},\"partII\":[{\"id\":\"VEHICLESAFETYEXT\",\"value\":{\"pathHistory\":{\"crumbData\":[{\"elevationOffset\":-19.8,\"latOffset\":7.55e-5,\"lonOffset\":0.0002609,\"timeOffset\":32.2},{\"elevationOffset\":-25.8,\"latOffset\":7.32e-5,\"lonOffset\":0.0003135,\"timeOffset\":34},{\"elevationOffset\":-34.5,\"latOffset\":0.0001027,\"lonOffset\":0.0004479,\"timeOffset\":37.2},{\"elevationOffset\":-128.2,\"latOffset\":0.000232,\"lonOffset\":0.0011832,\"timeOffset\":73.44}]},\"pathPrediction\":{\"confidence\":50,\"radiusOfCurve\":0}}},{\"id\":\"SUPPLEMENTALVEHICLEEXT\",\"value\":{\"classDetails\":{\"fuelType\":\"UNKNOWNFUEL\",\"hpmsType\":\"NONE\",\"keyType\":0,\"regional\":[],\"role\":\"BASICVEHICLE\"},\"regional\":[],\"vehicleData\":{\"bumpers\":{\"front\":0.5,\"rear\":0.6},\"height\":1.9},\"weatherProbe\":{},\"accelLong\":\"test\",\"accelLat\":\"test\",\"accelVert\":\"test\",\"yawRate\":\"test\",\"steeringWheelAngle\":\"test\",\"leftTurnSignalOn\":\"test\",\"rightTurnSignalOn\":\"test\",\"hazardSignalOn\":\"test\",\"fogLightOn\":\"test\",\"lowBeamHeadlightsOn\":\"test\",\"highBeamHeadlightsOn\":\"test\",\"automaticLightControlOn\":\"test\",\"daytimeRunningLightsOn\":\"test\",\"parkingLightsOn\":\"test\",\"wiperStatusFront\":\"test\",\"wiperStatusRear\":\"test\",\"wiperRateFront\":\"test\",\"wiperRateRear\":\"test\",\"eventAirBagDeployment\":\"test\",\"sunSensor\":\"test\",\"coefficientOfFriction\":\"test\",\"ambientAirTemperature\":\"test\",\"ambientAirPressure\":\"test\",\"transmissionState\":\"test\",\"vehicleSpeed\":\"test\",\"antiLockBrakeStatus\":\"test\",\"stabilityControlStatus\":\"test\",\"tractionControlStatus\":\"test\",\"brakeBoostApplied\":\"test\",\"brakeAppliedStatus\":\"test\",\"auxiliaryBrakeStatus\":\"test\",\"tirePressure\":\"test\",\"acceleratorPedalPosition\":\"test\",\"brakePedalPosition\":\"test\",\"disabledVehicle\":\"test\",\"stalledVehicle\":\"test\",\"airBagDeployment\":\"test\"}}]},\"dataType\":\"us.dot.its.jpo.ode.plugin.j2735.J2735Bsm\",\"schemaVersion\":1},\"schemaVersion\":1}";
@@ -1651,6 +1651,21 @@ TEST_CASE( "BSMHandler JSON PartII Redaction Only", "[ppm][redaction][partIIonly
     REQUIRE ( loadTestCases( "unit-test-data/test-case.partII.json", json_test_cases ) );
 
     for ( auto& test_case : json_test_cases ) {
+        // read into rapidjson document
+        rapidjson::Document doc;
+        rapidjson::ParseResult preliminaryParseResult = doc.Parse(test_case.c_str());
+        REQUIRE( preliminaryParseResult );
+
+        int numMembersPresentBeforeRedaction = 0;
+        for (std::string memberName : rpm.getFields()) {
+            bool found = false;
+            handler.getRapidjsonRedactor().searchForMemberByName(doc, memberName, found);
+            if (found) {
+                numMembersPresentBeforeRedaction++;
+            }
+        }
+        REQUIRE( numMembersPresentBeforeRedaction > 0 );
+
         // process test case to build BSM (redaction will occur here)
         CHECK( handler.process( test_case ) );
 
@@ -1666,12 +1681,16 @@ TEST_CASE( "BSMHandler JSON PartII Redaction Only", "[ppm][redaction][partIIonly
         rapidjson::ParseResult parseResult = partII.Parse(partIIString.c_str());
         CHECK( parseResult );
 
-        // verify that there are no sensitive members in the partII field left
-        for (std::string member : rpm.getFields()) {
+        // verify that there are no sensitive members in the coreData field left
+        int numMembersPresentAfterRedaction = 0;
+        for (std::string memberName : rpm.getFields()) {
             bool found = false;
-            handler.getRapidjsonRedactor().searchForMemberByName(partII, member, found);
-            CHECK( !found );
+            handler.getRapidjsonRedactor().searchForMemberByName(partII, memberName, found);
+            if (found) {
+                numMembersPresentAfterRedaction++;
+            }
         }
+        CHECK( numMembersPresentAfterRedaction == 0 );
     }
 
 }
@@ -1697,6 +1716,21 @@ TEST_CASE( "BSMHandler JSON PartII Redaction w/ All Flags", "[ppm][redaction][pa
     REQUIRE ( loadTestCases( "unit-test-data/test-case.partII.json", json_test_cases ) );
 
     for ( auto& test_case : json_test_cases ) {
+        // read into rapidjson document
+        rapidjson::Document doc;
+        rapidjson::ParseResult preliminaryParseResult = doc.Parse(test_case.c_str());
+        REQUIRE( preliminaryParseResult );
+
+        int numMembersPresentBeforeRedaction = 0;
+        for (std::string memberName : rpm.getFields()) {
+            bool found = false;
+            handler.getRapidjsonRedactor().searchForMemberByName(doc, memberName, found);
+            if (found) {
+                numMembersPresentBeforeRedaction++;
+            }
+        }
+        REQUIRE( numMembersPresentBeforeRedaction > 0 );
+
         // process test case to build BSM (redaction will occur here)
         CHECK( handler.process( test_case ) );
 
@@ -1712,12 +1746,16 @@ TEST_CASE( "BSMHandler JSON PartII Redaction w/ All Flags", "[ppm][redaction][pa
         rapidjson::ParseResult parseResult = partII.Parse(partIIString.c_str());
         CHECK( parseResult );
 
-        // verify that there are no sensitive members in the partII field left
-        for (std::string member : rpm.getFields()) {
+        // verify that there are no sensitive members in the coreData field left
+        int numMembersPresentAfterRedaction = 0;
+        for (std::string memberName : rpm.getFields()) {
             bool found = false;
-            handler.getRapidjsonRedactor().searchForMemberByName(partII, member, found);
-            CHECK( !found );
+            handler.getRapidjsonRedactor().searchForMemberByName(partII, memberName, found);
+            if (found) {
+                numMembersPresentAfterRedaction++;
+            }
         }
+        CHECK( numMembersPresentAfterRedaction == 0 );
     }
 
 }
@@ -1725,6 +1763,8 @@ TEST_CASE( "BSMHandler JSON PartII Redaction w/ All Flags", "[ppm][redaction][pa
 TEST_CASE( "BSMHandler JSON CoreData Redaction Only", "[ppm][redaction][coredataonly]" ) {
     // create redaction properties manager
     RedactionPropertiesManager rpm;
+
+    REQUIRE( rpm.getFields().size() > 0 );
 
     // create BSMHandler
     std::unordered_map<std::string,std::string> pconf;
@@ -1749,6 +1789,21 @@ TEST_CASE( "BSMHandler JSON CoreData Redaction Only", "[ppm][redaction][coredata
     REQUIRE ( loadTestCases( "unit-test-data/test-case.coreData.json", json_test_cases ) );
 
     for ( auto& test_case : json_test_cases ) {
+        // read into rapidjson document
+        rapidjson::Document doc;
+        rapidjson::ParseResult preliminaryParseResult = doc.Parse(test_case.c_str());
+        REQUIRE( preliminaryParseResult );
+
+        int numMembersPresentBeforeRedaction = 0;
+        for (std::string memberName : rpm.getFields()) {
+            bool found = false;
+            handler.getRapidjsonRedactor().searchForMemberByName(doc, memberName, found);
+            if (found) {
+                numMembersPresentBeforeRedaction++;
+            }
+        }
+        REQUIRE( numMembersPresentBeforeRedaction > 0 );
+
         // process test case to build BSM (redaction will occur here)
         CHECK( handler.process( test_case ) );
 
@@ -1765,17 +1820,23 @@ TEST_CASE( "BSMHandler JSON CoreData Redaction Only", "[ppm][redaction][coredata
         CHECK( parseResult );
 
         // verify that there are no sensitive members in the coreData field left
-        for (std::string member : rpm.getFields()) {
+        int numMembersPresentAfterRedaction = 0;
+        for (std::string memberName : rpm.getFields()) {
             bool found = false;
-            handler.getRapidjsonRedactor().searchForMemberByName(coreData, member, found);
-            CHECK( !found );
+            handler.getRapidjsonRedactor().searchForMemberByName(coreData, memberName, found);
+            if (found) {
+                numMembersPresentAfterRedaction++;
+            }
         }
+        CHECK( numMembersPresentAfterRedaction == 0 );
     }
 }
 
 TEST_CASE( "BSMHandler JSON CoreData Redaction w/ All Flags", "[ppm][redaction][coredata][allflags]" ) {
     // create redaction properties manager
     RedactionPropertiesManager rpm;
+
+    REQUIRE( rpm.getFields().size() > 0 );
 
     // create BSMHandler
     std::unordered_map<std::string,std::string> pconf;
@@ -1794,6 +1855,21 @@ TEST_CASE( "BSMHandler JSON CoreData Redaction w/ All Flags", "[ppm][redaction][
     REQUIRE ( loadTestCases( "unit-test-data/test-case.coreData.json", json_test_cases ) );
 
     for ( auto& test_case : json_test_cases ) {
+        // read into rapidjson document
+        rapidjson::Document doc;
+        rapidjson::ParseResult preliminaryParseResult = doc.Parse(test_case.c_str());
+        REQUIRE( preliminaryParseResult );
+
+        int numMembersPresentBeforeRedaction = 0;
+        for (std::string memberName : rpm.getFields()) {
+            bool found = false;
+            handler.getRapidjsonRedactor().searchForMemberByName(doc, memberName, found);
+            if (found) {
+                numMembersPresentBeforeRedaction++;
+            }
+        }
+        REQUIRE( numMembersPresentBeforeRedaction > 0 );
+        
         // process test case to build BSM (redaction will occur here)
         CHECK( handler.process( test_case ) );
 
@@ -1810,10 +1886,14 @@ TEST_CASE( "BSMHandler JSON CoreData Redaction w/ All Flags", "[ppm][redaction][
         CHECK( parseResult );
 
         // verify that there are no sensitive members in the coreData field left
-        for (std::string member : rpm.getFields()) {
+        int numMembersPresentAfterRedaction = 0;
+        for (std::string memberName : rpm.getFields()) {
             bool found = false;
-            handler.getRapidjsonRedactor().searchForMemberByName(coreData, member, found);
-            CHECK( !found );
+            handler.getRapidjsonRedactor().searchForMemberByName(coreData, memberName, found);
+            if (found) {
+                numMembersPresentAfterRedaction++;
+            }
         }
+        CHECK( numMembersPresentAfterRedaction == 0 );
     }
 }
