0c0cba29c5d49dbd06b7efe0ac738b3d37727d20
Daniel McCoy Stephenson <dmccoystephenson@gmail.com>
2023-01-20 14:10:54 -0700

    Utilized general redaction test case in RapidjsonRedactor redact-by-path test.

--- src/tests.cpp
+++ src/tests.cpp
@@ -1579,84 +1579,64 @@ TEST_CASE( "RapidjsonRedactor Redact All Instances Of Member By Name", "[ppm][re
 TEST_CASE( "RapidjsonRedactor Redact Member By Path", "[ppm][redaction][rapidjsonredactor][redactbypath]" ) {
     RapidjsonRedactor rapidjsonRedactor;
 
-    const char* jsonString = "{\"metadata\":{\"latency\":1,\"logFileName\":\"wsmpforward.coer\",\"payloadType\":\"us.dot.its.jpo.ode.model.OdeBsmPayload\",\"receivedAt\":\"2017-08-02T19:56:45.822Z[UTC]\",\"sanitized\":false,\"schemaVersion\":1,\"serialId\":{\"bundleId\":4,\"bundleSize\":1,\"recordId\":2,\"serialNumber\":0,\"streamId\":\"0bfda39b-0bf1-4e2e-a1f1-b858426f7408\"},\"validSignature\":false},\"payload\":{\"data\":{\"coreData\":{\"accelSet\":{\"accelYaw\":0},\"accuracy\":{\"semiMajor\":12.7,\"semiMinor\":12.7},\"brakes\":{\"abs\":\"unavailable\",\"auxBrakes\":\"unavailable\",\"brakeBoost\":\"unavailable\",\"scs\":\"unavailable\",\"traction\":\"unavailable\",\"wheelBrakes\":{\"leftFront\":false,\"leftRear\":false,\"rightFront\":false,\"rightRear\":false,\"unavailable\":true}},\"heading\":321.0125,\"id\":\"G1\",\"msgCnt\":1,\"position\":{\"elevation\":154.7,\"latitude\":35.94911,\"longitude\":-83.928343},\"secMark\":36799,\"size\":{\"length\":250,\"width\":150},\"speed\":22.1},\"partII\":[{\"id\":\"VEHICLESAFETYEXT\",\"value\":{\"pathHistory\":{\"crumbData\":[{\"elevationOffset\":-19.8,\"latOffset\":7.55e-5,\"lonOffset\":0.0002609,\"timeOffset\":32.2},{\"elevationOffset\":-25.8,\"latOffset\":7.32e-5,\"lonOffset\":0.0003135,\"timeOffset\":34},{\"elevationOffset\":-34.5,\"latOffset\":0.0001027,\"lonOffset\":0.0004479,\"timeOffset\":37.2},{\"elevationOffset\":-128.2,\"latOffset\":0.000232,\"lonOffset\":0.0011832,\"timeOffset\":73.44}]},\"pathPrediction\":{\"confidence\":50,\"radiusOfCurve\":0}}},{\"id\":\"SUPPLEMENTALVEHICLEEXT\",\"value\":{\"classDetails\":{\"fuelType\":\"UNKNOWNFUEL\",\"hpmsType\":\"NONE\",\"keyType\":0,\"regional\":[],\"role\":\"BASICVEHICLE\"},\"regional\":[],\"vehicleData\":{\"bumpers\":{\"front\":0.5,\"rear\":0.6},\"height\":1.9},\"weatherProbe\":{},\"accelLong\":\"test\",\"accelLat\":\"test\",\"accelVert\":\"test\",\"yawRate\":\"test\",\"steeringWheelAngle\":\"test\",\"leftTurnSignalOn\":\"test\",\"rightTurnSignalOn\":\"test\",\"hazardSignalOn\":\"test\",\"fogLightOn\":\"test\",\"lowBeamHeadlightsOn\":\"test\",\"highBeamHeadlightsOn\":\"test\",\"automaticLightControlOn\":\"test\",\"daytimeRunningLightsOn\":\"test\",\"parkingLightsOn\":\"test\",\"wiperStatusFront\":\"test\",\"wiperStatusRear\":\"test\",\"wiperRateFront\":\"test\",\"wiperRateRear\":\"test\",\"eventAirBagDeployment\":\"test\",\"sunSensor\":\"test\",\"coefficientOfFriction\":\"test\",\"ambientAirTemperature\":\"test\",\"ambientAirPressure\":\"test\",\"transmissionState\":\"test\",\"vehicleSpeed\":\"test\",\"antiLockBrakeStatus\":\"test\",\"stabilityControlStatus\":\"test\",\"tractionControlStatus\":\"test\",\"brakeBoostApplied\":\"test\",\"brakeAppliedStatus\":\"test\",\"auxiliaryBrakeStatus\":\"test\",\"tirePressure\":\"test\",\"acceleratorPedalPosition\":\"test\",\"brakePedalPosition\":\"test\",\"disabledVehicle\":\"test\",\"stalledVehicle\":\"test\",\"airBagDeployment\":\"test\"}}]},\"dataType\":\"us.dot.its.jpo.ode.plugin.j2735.J2735Bsm\",\"schemaVersion\":1},\"schemaVersion\":1}";
-    rapidjson::Document document = rapidjsonRedactor.getDocumentFromString(jsonString);
+    // load in test cases
+    std::vector<std::string> json_test_cases;
+    REQUIRE ( loadTestCases( "unit-test-data/test-case.redaction.general.json", json_test_cases ) );
+    for (auto& jsonString : json_test_cases) {
+        rapidjson::Document document = rapidjsonRedactor.getDocumentFromString(jsonString);
 
-    std::string memberPaths[] = { 
-                            "payload.data.partII.value.accelLong",
-                            "payload.data.partII.value.accelLat",
-                            "payload.data.partII.value.accelVert",
-                            "payload.data.partII.value.yawRate",
-                            "payload.data.partII.value.steeringWheelAngle",
-                            "payload.data.partII.value.leftTurnSignalOn",
-                            "payload.data.partII.value.rightTurnSignalOn",
-                            "payload.data.partII.value.hazardSignalOn",
-                            "payload.data.partII.value.fogLightOn",
-                            "payload.data.partII.value.lowBeamHeadlightsOn",
-                            "payload.data.partII.value.highBeamHeadlightsOn",
-                            "payload.data.partII.value.automaticLightControlOn",
-                            "payload.data.partII.value.daytimeRunningLightsOn",
-                            "payload.data.partII.value.parkingLightsOn",
-                            "payload.data.partII.value.wiperStatusFront",
-                            "payload.data.partII.value.wiperStatusRear",
-                            "payload.data.partII.value.wiperRateFront",
-                            "payload.data.partII.value.wiperRateRear",
-                            "payload.data.partII.value.eventAirBagDeployment",
-                            "payload.data.partII.value.sunSensor",
-                            "payload.data.partII.value.coefficientOfFriction",
-                            "payload.data.partII.value.ambientAirTemperature",
-                            "payload.data.partII.value.ambientAirPressure",
-                            "payload.data.partII.value.transmissionState",
-                            "payload.data.partII.value.vehicleSpeed",
-                            "payload.data.partII.value.antiLockBrakeStatus",
-                            "payload.data.partII.value.stabilityControlStatus",
-                            "payload.data.partII.value.tractionControlStatus",
-                            "payload.data.partII.value.brakeBoostApplied",
-                            "payload.data.partII.value.brakeAppliedStatus",
-                            "payload.data.partII.value.auxiliaryBrakeStatus",
-                            "payload.data.partII.value.tirePressure",
-                            "payload.data.partII.value.acceleratorPedalPosition",
-                            "payload.data.partII.value.brakePedalPosition",
-                            "payload.data.partII.value.disabledVehicle",
-                            "payload.data.partII.value.stalledVehicle",
-                            "payload.data.partII.value.airBagDeployment",
-                        };
+        std::string memberPaths[] = {
+            "payload.data.coreData.brakes.abs",
+            "payload.data.coreData.brakes.scs",
+            "payload.data.coreData.brakes.traction",
+            "payload.data.coreData.brakes.brakeBoost",
+            "payload.data.coreData.brakes.wheelBrakes.leftFront",
+            "payload.data.coreData.brakes.wheelBrakes.rightFront",
+            "payload.data.coreData.brakes.wheelBrakes.unavailable",
+            "payload.data.coreData.brakes.wheelBrakes.leftRear",
+            "payload.data.coreData.brakes.wheelBrakes.rightRear",
+            "payload.data.coreData.brakes.auxBrakes",
+        };
 
-    // get num members present before redaction
-    int numMembersPresentBeforeRedaction = 0;
-    for (int i = 0 ; i < sizeof(memberPaths)/sizeof(memberPaths[0]); i++) {
-        std::string memberPath = memberPaths[i];
+        // get num members present before redaction
+        int numMembersPresentBeforeRedaction = 0;
+        for (int i = 0 ; i < sizeof(memberPaths)/sizeof(memberPaths[0]); i++) {
+            std::string memberPath = memberPaths[i];
 
-        bool success = false;
-        rapidjsonRedactor.searchForMemberByPath(document, memberPath, success);
+            bool success = false;
+            rapidjsonRedactor.searchForMemberByPath(document, memberPath, success);
 
-        numMembersPresentBeforeRedaction += success;
+            numMembersPresentBeforeRedaction += success;
+            if (!success) {
+                std::cout << "did not find " << memberPath << std::endl;
+            }
+        }
+        REQUIRE(numMembersPresentBeforeRedaction == sizeof(memberPaths)/sizeof(memberPaths[0]));
+
+        // redact members
+        int numMembersRedacted = 0;
+        for (int i = 0 ; i < sizeof(memberPaths)/sizeof(memberPaths[0]); i++) {
+            std::string memberPath = memberPaths[i];
+
+            bool success = false;
+            rapidjsonRedactor.redactMemberByPath(document, memberPath, success);
+
+            numMembersRedacted += success;
+        }
+        REQUIRE(numMembersRedacted == numMembersPresentBeforeRedaction);
+
+        // get num members present after redaction
+        int numMembersPresentAfterRedaction = 0;
+        for (int i = 0 ; i < sizeof(memberPaths)/sizeof(memberPaths[0]); i++) {
+            std::string memberPath = memberPaths[i];
+
+            bool success = false;
+            rapidjsonRedactor.searchForMemberByPath(document, memberPath, success);
+
+            numMembersPresentAfterRedaction += success;
+        }
+        REQUIRE(numMembersPresentAfterRedaction == 0);
     }
-    REQUIRE(numMembersPresentBeforeRedaction == sizeof(memberPaths)/sizeof(memberPaths[0]));
-
-    // redact members
-    int numMembersRedacted = 0;
-    for (int i = 0 ; i < sizeof(memberPaths)/sizeof(memberPaths[0]); i++) {
-        std::string memberPath = memberPaths[i];
-
-        bool success = false;
-        rapidjsonRedactor.redactMemberByPath(document, memberPath, success);
-
-        numMembersRedacted += success;
-    }
-    REQUIRE(numMembersRedacted == numMembersPresentBeforeRedaction);
-
-    // get num members present after redaction
-    int numMembersPresentAfterRedaction = 0;
-    for (int i = 0 ; i < sizeof(memberPaths)/sizeof(memberPaths[0]); i++) {
-        std::string memberPath = memberPaths[i];
-
-        bool success = false;
-        rapidjsonRedactor.searchForMemberByPath(document, memberPath, success);
-
-        numMembersPresentAfterRedaction += success;
-    }
-    REQUIRE(numMembersPresentAfterRedaction == 0);
 }
 
 TEST_CASE( "BSMHandler JSON General Redaction Only", "[ppm][redaction][generalonly]" ) {
