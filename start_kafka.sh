8e7c4c9ecf1b4ba769095372cf0f60defff35a1d
dmccoystephenson <dmccoystephenson@gmail.com>
2024-09-05 12:20:28 -0600

    Changed `docker-compose` to `docker compose` in kafka scripts

--- start_kafka.sh
+++ start_kafka.sh
@@ -3,4 +3,4 @@
 ./stop_kafka.sh
 
 # start kafka
-docker-compose -f docker-compose-kafka.yml up -d
+docker compose -f docker-compose-kafka.yml up -d
--- stop_kafka.sh
+++ stop_kafka.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # stop kafka
-docker-compose -f docker-compose-kafka.yml down --remove-orphans
+docker compose -f docker-compose-kafka.yml down --remove-orphans
